a936bba55ce7dbd333bd8825164341395424023c
Eglany Junior <contas.eglanyjr@gmail.com>
2021-11-17 18:05:16 -0400

    feat: login validation, render Header, loading

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Link to="/">Login</Link>
-          <Link to="/search">Search</Link>
-          <Link to="/album">Album</Link>
-          <Link to="/favorites">Favorites</Link>
-          <Link to="/profile">Prodile</Link>
-          <Link to="/Profile/edit">Profile Edit</Link>
-          <Link to="*" />
-        </div>
-      </BrowserRouter>
+      <nav data-testid="header-component">
+        <Link to="/search">Search</Link>
+        <Link to="/album">Album</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/profile">Prodile</Link>
+        <Link to="/Profile/edit">Profile Edit</Link>
+      </nav>
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../componets/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      buttonLogin: true,
+      loading: false,
+      loginUser: false,
+    };
+  }
+
+  onInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, this.validateLogin);
+  }
+
+  validateLogin = () => {
+    const { userName } = this.state;
+    const minCharactes = 3;
+
+    this.setState({ buttonLogin: userName.length < minCharactes });
+  }
+  // https://stackoverflow.com/questions/43718103/arrow-functions-with-async-and-await-in-react-native
+
+  onLoginButton = async () => {
+    const { userName } = this.state;
+    this.setState({ loading: true });
+    await createUser(userName);
+    this.setState({
+      loading: false,
+      loginUser: true,
+    });
+
+    console.log('chamou');
+  }
+
+  // https://stackoverflow.com/questions/45089386/what-is-the-best-way-to-redirect-a-page-using-react-router
   render() {
+    const { userName, buttonLogin, loading, loginUser } = this.state;
+    if (loading) return <Loading />;
+    if (loginUser) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
-        Login
+        <h3>Tela de Login</h3>
+        <input
+          name="userName"
+          type="text"
+          value={ userName }
+          onChange={ this.onInputChange }
+          data-testid="login-name-input"
+        />
+        <button
+          name="buttonLogin"
+          type="button"
+          onClick={ this.onLoginButton }
+          disabled={ buttonLogin }
+          data-testid="login-submit-button"
+        >
+          Login
+        </button>
       </div>
     );
   }
